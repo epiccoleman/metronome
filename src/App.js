052c6dff795125660a0dae90ccb9f4f9528fc719
Eric Coleman <eric@epiccoleman.com>
2019-09-18 12:38:44 -0400

    functioning metronome using tone.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import Tone, { Sampler } from "tone";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Tick from "./tick.wav"
 
-export default App;
+export default class App extends React.Component {
+
+   constructor(props) {
+    super(props);
+    this.state = { isLoaded: false, isPlaying: false, bpm: 80};
+
+    this.sampler = new Sampler(
+      { A1: Tick },
+      {
+        onload: () => {
+          this.setState({ isLoaded: true });
+        }
+      }
+    ).toMaster();
+
+    Tone.Transport.bpm.value = this.state.bpm;
+  }
+
+  handleStart = () => {
+    var loop = new Tone.Loop((time) => {
+      //triggered every eighth note. 
+      console.log(time);
+      this.sampler.triggerAttack("A1");
+    }, "4n").start(0);
+
+    Tone.Transport.start();
+    this.setState({isPlaying: true});
+  }
+
+  handleStop = () => {
+    Tone.Transport.stop();
+    this.setState({isPlaying: false});
+  }
+
+  changeTempo = (change) => {
+    this.setState((prevState) => {
+      let newBpm = prevState.bpm + change
+      Tone.Transport.bpm.value = newBpm;
+
+      return {
+        bpm: newBpm
+      }
+    });
+  }
+
+  render() {
+    const { isLoaded, isPlaying } = this.state;
+    return (
+      <div>
+        <button disabled={!isLoaded || isPlaying} onClick={this.handleStart}>
+          start
+        </button>
+        <button disabled={!isPlaying} onClick={this.handleStop}>stop</button>
+        <div>
+          <button onClick={() => {this.changeTempo(-1)}}>-</button>
+          <div>{this.state.bpm}</div>
+          <button onClick={() => {this.changeTempo(1)}}>+</button>
+        </div>
+      </div>
+    );
+  }
+}
