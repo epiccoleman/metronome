3d4dd1663da8ae73bcd47e92bb84eeb63607d867
Eric Coleman <eric@epiccoleman.com>
2019-09-21 16:37:07 -0400

    Updates

---
+++ precache-manifest.70e8a86b93fe1ee40405c0584b32eef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c5b569e71f0a748146abd5d9117c11",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "a9bcac67bb8f20813514",
+    "url": "/metronome/static/css/main.bd0f6a75.chunk.css"
+  },
+  {
+    "revision": "b108e78fd98df65b9b38",
+    "url": "/metronome/static/js/2.5261f29d.chunk.js"
+  },
+  {
+    "revision": "a9bcac67bb8f20813514",
+    "url": "/metronome/static/js/main.4bce8ba3.chunk.js"
+  },
+  {
+    "revision": "c6e4465cd62e4ff6b492",
+    "url": "/metronome/static/js/runtime~main.940e3d7e.js"
+  },
+  {
+    "revision": "cd76df3c7235edaedbc7775a29977c2d",
+    "url": "/metronome/static/media/tick.cd76df3c.wav"
+  }
+]);
--- src/notes.md
+++
@@ -1,7 +0,0 @@
-Color scheme: 
-
-#61892f  darker green
-#86c232  light green
-#222629  black
-#474b4f  dk grey
-#6b6e70  lt grey
